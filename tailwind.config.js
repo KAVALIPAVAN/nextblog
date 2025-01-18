a5e677c2ac077461a4982733a1af9e34237e9cc8
KAVALI PAVAN <kavalipavan22@gmail.com>
2025-01-18 11:12:23 +0530

    05

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,6 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
-    // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
@@ -16,15 +14,25 @@ module.exports = {
       },
     },
   },
-    safelist: [
-      'bg-black', 
-      'text-white', 
-      'py-1', 
-      'px-4', 
-      'rounded-sm',
-      'flex', 
-      'justify-center', 
-      'items-center',
-    ],
+  safelist: [
+    'bg-black', 
+    'text-white', 
+    'py-1', 
+    'px-4', 
+    'rounded-sm', 
+    'flex', 
+    'justify-center', 
+    'items-center', 
+    'text-center', 
+    'object-cover', // Added for images
+    'max-w-[330px]', 'sm:max-w-[300px]', // Added for specific max-width classes
+    'hover:shadow-[-7px_7px_0px_#000000]', // For hover shadow
+    'border', 'border-black', 'shadow-[-7px_7px_0px_#000000]', // For borders and shadows
+    'gap-1', 'gap-y-10', 'xl:mx-24', // Gap classes
+    'hidden', 'sm:block', 'sm:w-[80%]', // Hidden for small screens
+    'max-w-[500px]', 'scale-75', 'sm:scale-100', // For scaling and max-width in forms
+    'mb-16', 'sm:text-base', 'text-xs', 'text-lg', // Adjusted text sizes
+    'sm:w-auto', 'sm:w-[130px]', 'w-[120px]', // Adjusted responsive widths
+  ],
   plugins: [],
 };
