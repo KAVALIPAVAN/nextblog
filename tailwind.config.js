873158d5d02f952cddbe4bb1a4aacc5c1d7d9e39
KAVALI PAVAN <kavalipavan22@gmail.com>
2025-01-18 11:25:15 +0530

    06

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,15 +24,12 @@ module.exports = {
     'justify-center', 
     'items-center', 
     'text-center', 
-    'object-cover', // Added for images
-    'max-w-[330px]', 'sm:max-w-[300px]', // Added for specific max-width classes
-    'hover:shadow-[-7px_7px_0px_#000000]', // For hover shadow
-    'border', 'border-black', 'shadow-[-7px_7px_0px_#000000]', // For borders and shadows
-    'gap-1', 'gap-y-10', 'xl:mx-24', // Gap classes
-    'hidden', 'sm:block', 'sm:w-[80%]', // Hidden for small screens
-    'max-w-[500px]', 'scale-75', 'sm:scale-100', // For scaling and max-width in forms
-    'mb-16', 'sm:text-base', 'text-xs', 'text-lg', // Adjusted text sizes
-    'sm:w-auto', 'sm:w-[130px]', 'w-[120px]', // Adjusted responsive widths
+    'object-cover', 
+    'border', 'border-black', 'shadow-[-7px_7px_0px_#000000]', 
+    'gap',
+    'hidden', 'sm:block', 
+     'scale', 
+ 
   ],
   plugins: [],
 };
